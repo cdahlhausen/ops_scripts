5ec8be9b618ebdaf12fc99c75855b252c2ea7cd0
Christian Dahlhausen <cd3ef@virginia.edu>
2015-10-19 16:28:56 -0400

    Remove whitespaces.

--- diskcheck.py
+++ diskcheck.py
@@ -1,44 +1,44 @@
 #!/usr/bin/env python
 
-# Please don't judge this code. It's not good I know. 
+# Please don't judge this code. It's not good I know.
 # I ignored PEP8 for this.
-import smtplib 
+import smtplib
 import os
-import socket 
+import socket
 import statvfs
 import sys
 from time import localtime, strftime
 
 #Get servers hostname
-SRV = str(socket.gethostname()) 
+SRV = str(socket.gethostname())
 
-# CONFIGURE HERE	
-SERVER = "localhost"	
-PORT = '25' # If left blank port 25 is assumed.	
-# Mail	
-#SEND_FROM = ('ops@' + SRV)	
-SEND_FROM = ('ops@aptrust.org')	
-SEND_TO = ['ops@aptrust.org']	
-#Monitor	
-MON0='/'	
-#MON1=''	
+# Mail CONFIGURATION
+SERVER = "localhost"
+PORT = '25' # If left blank port 25 is assumed.
+SEND_FROM = ('ops@' + SRV)
+SEND_FROM = ('SRV@aptrust.org')
+SEND_TO = ['ops@aptrust.org']
+
+# Locations to monitor
+MON0='/'
+#MON1=''
 #MON2=''
-MON_ARRAY = (MON0) 
+MON_ARRAY = (MON0)
 # Limit in MiB
-LIMIT=5000;	
-# END OF CONFIG	
+LIMIT=2000;
+# END OF CONFIG
 
 #Define current Time as Fri, 04 Mar 2011 08:47:10
 TIME = strftime("%a, %d %b %Y %H:%M:%S", localtime())
 
 
 for disk in MON_ARRAY:
-	MONITOR = os.statvfs(str(disk)) 
-	SPACE_LEFT=(MONITOR.f_bavail * MONITOR.f_frsize) / 1048576 #MiB 
-	
+	MONITOR = os.statvfs(str(disk))
+	SPACE_LEFT=(MONITOR.f_bavail * MONITOR.f_frsize) / 1048576 #MiB
+
 	if (SPACE_LEFT < LIMIT):
 		STATUS = "- is NOT OK on "
-		MSG = ("{}  {} has only {}MB left on {}").format(TIME, SRV, SPACE_LEFT,disk) 
+		MSG = ("{}  {} has only {}MB left on {}").format(TIME, SRV, SPACE_LEFT,disk)
 		smtpserver = smtplib.SMTP(SERVER, PORT)
 		smtpserver.ehlo(SRV)
 
@@ -47,7 +47,7 @@ for disk in MON_ARRAY:
 		# Uncomment smtpserver.login() if login is required
 		#smtpserver.login('', '')
 
-		HEADER = 'To:' + str(SEND_TO) + '\n' + 'From: ' + str(SEND_FROM) + '\n' + 'Subject:Alert - Low disk space on ' + SRV + '\n' 
+		HEADER = 'To:' + str(SEND_TO) + '\n' + 'From: ' + str(SEND_FROM) + '\n' + 'Subject:Alert - Low disk space on ' + SRV + '\n'
 		MAIL = HEADER + str(MSG)
 
 		smtpserver.sendmail(SEND_FROM, SEND_TO, MAIL)
@@ -58,8 +58,6 @@ for disk in MON_ARRAY:
 	else:
 		print """
 		A serious problem detected with the script.
-		Please check what mount points you monitor and check that 
+		Please check what mount points you monitor and check that
 		they are in the MON_ARRAY aswell.
 		"""
-
-
